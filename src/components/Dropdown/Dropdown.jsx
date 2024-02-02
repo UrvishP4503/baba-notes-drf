e4d37191c8a02ffe42dbe0e203147de2865a2d78
Patel Het <125752460+hetpatel2508@users.noreply.github.com>
2024-02-02 22:53:28 +0530

    New Changes By Het Patel (#2)
    Something related to frontend

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -15,20 +15,40 @@ const Dropdown = () => {
         bobobo is {isOpen ? "open" : "closed"}
       </button>
 
-      <div className="dropdown-menu">
+      {/* <div className="dropdown-menu">
         {isOpen && (
           // TODO: remove the part below and replace it with the actual categories from the backend.
           <div>
-            <InputButtonComponent />
+            <div>
+            <InputButtonComponent/>
+            </div>
+            <div>
             <ul>
               <li>1</li>
               <li>2</li>
               <li>3</li>
               <li>4</li>
             </ul>
+            </div>
           </div>
         )}
-      </div>
+      </div> */}
+      {isOpen && (
+        <div className="dropdown-add-main">
+
+          <div className="dropdown-add-semi-main-1">
+            <input type="text" placeholder="New Categories :" className="dropdown-add-input"/>
+            <button className="dropdown-add-button">ADD</button>
+          </div>
+          <div className="dropdown-add-semi-main-2">
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+            <div className="dropdown-category-container"><div className="dropdown-category-hashtag">  #</div><div className="dropdown-category-name">WWWWWWWWWWWWWWW</div></div>
+          </div>
+        </div>)}
     </div>
   );
 };
@@ -46,12 +66,14 @@ const InputButtonComponent = () => {
   };
 
   return (
-    <div>
+    <div style={{display:"flex" , flexDirection:"column" , gap:"7px"}}>
       <input
         type="text"
         value={userInput}
+        
         onChange={handleInputChange}
         placeholder="Type something..."
+        style={{width:"70%" , height:"25px"}}
       />
       <button onClick={handleButtonPress}>Press me</button>
 
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -6,7 +6,8 @@ const Notes = () => {
   return temp.map(data => {
     return (
       <div className="box" key={data.title}>
-        <div className="title">{data.title}</div>
+        <div className="title-text">{data.title}</div>
+        {/* <div style={{color:"white"}}>⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</div> */}
         <p className="note-text">{data.noteText}</p>
         <div className="created-at">{data.created_at}</div>
       </div>
