2a2b074f9561a48808d6a5e0bd44b201a876d0b1
Urvish Patel <92674497+UrvishP4503@users.noreply.github.com>
2024-02-14 22:41:15 +0530

    ...

--- frontend/src/components/NewNote/NewNote.tsx
+++ frontend/src/components/NewNote/NewNote.tsx
@@ -1,12 +1,41 @@
+import Dropdown from "../Dropdown/Dropdown";
 import "./NewNote.css";
+
+interface NewNote {
+  title: string;
+  // category: string;
+  note: string;
+}
+
+async function saveNote(noteData: NewNote): Promise<void> {
+  // todo: save note to database
+  console.log(noteData);
+}
+
+const handleSave = () => {
+  const title = document.querySelector(".title") as HTMLInputElement;
+  const note = document.querySelector("textarea") as HTMLTextAreaElement;
+
+  const noteData: NewNote = {
+    title: title.value,
+    note: note.value,
+  };
+
+  saveNote(noteData);
+};
 
 const NewNote = () => {
   return (
     <div className="new-note">
       <div className="head">
         <input type="text" className="title" placeholder="Enter Title"></input>
-        <div className="category">hi</div>
-        <button className="save-button"> Save </button>
+        <div className="selected-category"> selected Category</div>
+        <div className="dropdown-container">
+          <Dropdown />
+        </div>
+        <button className="save-button" onClick={handleSave}>
+          Save
+        </button>
       </div>
 
       <textarea
